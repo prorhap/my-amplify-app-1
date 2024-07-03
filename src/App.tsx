b77af6aaafc3e7a412a835043a5de2458bc12dd7
choichul2 <choichul+demo@amazon.com>
2024-07-03 13:52:12 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
-import { useState, useEffect } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setMemos] = useState<Schema["Memo"]["type"][]>([]);
-
-  const fetchMemos = async () => {
-    const { data: items, errors } = await client.models.Memo.list();
-    if (!errors) {
-      setMemos(items);
-    }
-  };
+function App() {
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetchMemos();
+    const fetchData = async () => {
+      const response = await fetch('https://al3fbe8b17.execute-api.ap-northeast-2.amazonaws.com/Prod/hello');
+      const data = await response.json();
+      console.log(data);
+      setMessage(data.message);
+    };
+    fetchData();
   }, []);
 
-  const createMemo = async () => {
-    const content = window.prompt("Memo");
-    if (content) {
-      await client.models.Memo.create({
-        content
-      });
-      fetchMemos();
-    }
-  };
-
   return (
-    <div className="container">
-      <button className="button" onClick={createMemo}>Add new memo</button>
-      <ul className="list">
-        {todos.map(({ id, content }) => (
-          <li className="list-item" key={id}>{content}</li>
-        ))}
-      </ul>
+    <div className="App">
+      <div className="container">
+        <div className="message">{message}</div>
+      </div>
     </div>
   );
 }
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import outputs from './amplify_outputs.json';
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure(outputs);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
